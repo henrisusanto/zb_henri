d1e55727bd0791c1cebfa1fadaa9954f4714c9ca
henrisusanto <henry_qhj@yahoo.com>
2014-11-28 21:08:53 +0700

    sorting done

--- classes/sorting.php
+++ classes/sorting.php
@@ -12,60 +12,48 @@ class sorting {
     // PRODUCT CATEGORY > PRODUCT LINE > PRODUCT DISPLAY
     var $product_line = array(
         'taxonomy_name' => 'zb_product_line',
-        'sorting_field' => 'field_pline_1field_sorting',
-        'sorting_type_field' => 'field_pline_1bool_sorttype',
-        'natural_fields' => array('title' => 'Product Display Title')
+        'sorting_field' => 'field_tpl_1f_sorting',
+        'sorting_type_field' => 'field_tpl_1f_sortype',
     );
     var $product_category = array(
         'taxonomy_name' => 'zb_product_category',
-        'sorting_field' => 'field_pcat_1field_sorting',
-        'sorting_type_field' => 'field_pcat_1bool_sorttype',
-        'views_machine_name' => 'list_of_product_line',
+        'sorting_field' => 'field_tc_1f_sorting',
+        'sorting_type_field' => 'field_tc_1f_sorttype',
     );
     var $product_display = array(
-        'content_type_name' => 'zb_product_display'
+        'content_type_name' => 'zb_product_display',
+        'natural_fields' => array('title' => 'Product Display Title')
     );
 
-    function updateProductLineSortingOptions() {
-        $allowed_list = array();
-        foreach (field_info_instances('node', $this->product_display['content_type_name']) as $key => $value)
-            $allowed_list[$key] = $value['label'];
-        
-        foreach($this->product_line['natural_fields'] as $field => $label)
-            $allowed_list[$field] = $label;
-        
-        $info = field_info_field($this->product_line['sorting_field']);
-        $values = &$info['settings']['allowed_values'];
-        $values = $allowed_list;
-        field_update_field($info);
+    function updateSortingOptions() {
+        $options = $this->getProductDisplayFields();
+        $this->updateCategorySortingOption($options);
+        $this->updateProductLineSortingOption($options);
+        drupal_set_message('Sorting options updated successfully'.  json_encode($options));
     }
 
-    function updateProductCategorySortingOptions() {
-        $allowed_list = array();
-        foreach (field_info_instances('taxonomy_term', $this->product_line['taxonomy_name']) as $key => $value)
-            if ($key != $this->product_line['sorting_field'] &&
-                    $key != $this->product_line['sorting_type_field']
-            )
-                $allowed_list[$key] = $value['label'];
-        $info = field_info_field($this->product_category['sorting_field']);
-        $values = &$info['settings']['allowed_values'];
-        $values = $allowed_list;
-        field_update_field($info);
-    }
-
-    function getProductLineSortingParam($term) {
-        $retrieved_field = field_get_items('taxonomy_term', $term, $this->product_line['sorting_field']);
-        $sorting_field = $retrieved_field[0]['value'];
-        $retrieved_field = field_get_items('taxonomy_term', $term, $this->product_line['sorting_type_field']);
+    /*
+     * preparing parameters for sorting pusposes in CUSTOM_MODULE_NAME.views.inc
+     * @arg term object, taxonomy config ($product_line or $product_category )
+     * @return array containing : 
+     *      views_join object
+     *      sorting base (field name)
+     *      sorting type (ASC/DESC)
+     */
+    function getSortingParameter($term, $taxonomy) {
+        $retrieved_field = field_get_items('taxonomy_term', $term, $taxonomy['sorting_field']);
+        if(!$retrieved_field) return null;
+        else $sorting_field = $retrieved_field[0]['value'];
+        $retrieved_field = field_get_items('taxonomy_term', $term, $taxonomy['sorting_type_field']);
+        $sorting_type = $retrieved_field[0]['value'] ? 'ASC' : 'DESC';
 
         $param = array(
             'join' => null,
             'field' => $sorting_field,
-            'direction' => $retrieved_field[0]['value'] ? 'ASC' : 'DESC',
+            'direction' => $sorting_type,
         );
 
-        if ($sorting_field == '0') $param = null;
-        else if (!in_array($sorting_field, array_keys($this->product_line['natural_fields']))) {
+        if (!in_array($sorting_field, array_keys($this->product_display['natural_fields']))) {
             $table_fields = db_query("SELECT `COLUMN_NAME` FROM `INFORMATION_SCHEMA`.`COLUMNS` WHERE `TABLE_NAME`='field_data_$sorting_field'")->fetchAll();
             $foregin_key_relation = end($table_fields);
 
@@ -81,8 +69,41 @@ class sorting {
         }
         return $param;
     }
-
-    function getProductCategorySortingParam($term) {
+    
+    /*
+     * retrieve all fields of product display content type
+     * @return array field_name => field label
+     */
+    function getProductDisplayFields(){
+        $field_list = array();
+        foreach (field_info_instances('node', $this->product_display['content_type_name']) as $key => $value)
+            $field_list[$key] = $value['label'];
+        foreach($this->product_display['natural_fields'] as $field => $label)
+            $field_list[$field] = $label;
+        return $field_list;
     }
-
+    
+    /*
+     * provide list of product display fields as sorting option in each product category
+     * @arg list of options as array
+     * @action update field setting
+     */
+    function updateCategorySortingOption($options){
+        $info = field_info_field($this->product_category['sorting_field']);
+        $values = &$info['settings']['allowed_values'];
+        $values = $options;
+        field_update_field($info);
+    }
+    
+    /*
+     * provide list of product display fields as sorting option in each Product Line
+     * @arg list of options as array
+     * @action update field setting
+     */
+    function updateProductLineSortingOption($options){
+        $info = field_info_field($this->product_line['sorting_field']);
+        $values = &$info['settings']['allowed_values'];
+        $values = $options;
+        field_update_field($info);
+    }
 }
